f2250cff5880f6ce0a9b8256fa81ccd5308a97a9
Dziaduch <48856951+n0tcnj1eck4h@users.noreply.github.com>
2023-03-21 11:17:18 +0100

    Use a queue to bypass recursion limits

--- grid.py
+++ grid.py
@@ -6,6 +6,9 @@ class Grid:
     width: int
     height: int
     grid: list[set[Tile]]
+    propagation_queue: list[tuple[int, int]]
+    sorted_grid: list[set[Tile]]
+    wrap_around: bool
 
     def __init__(self, width: int, height: int, tiles: set[Tile]) -> None:
         self.width = width
@@ -14,9 +17,27 @@ class Grid:
         for _ in range(height):
             for _ in range(width):
                 self.grid.append(tiles.copy())
+        self.sorted_grid = sorted(self.grid, key=len)
+        self.propagation_queue = []
+        self.wrap_around = True
 
     def get(self, x: int, y: int) -> set[Tile]:
         return self.grid[y * self.width + x]
+
+    def observe_least_entropy(self) -> bool:
+        self.sorted_grid.sort(key=len)
+
+        # Pop all collapsed cells
+        while len(self.sorted_grid) != 0 and len(self.sorted_grid[0]) <= 1:
+            self.sorted_grid.pop(0)
+
+        if len(self.sorted_grid) == 0:
+            return False
+
+        option = self.sorted_grid.pop(0)
+        i = self.grid.index(option)
+        self.observe(i % self.width, i // self.width)
+        return True
 
     def observe(self, x: int, y: int) -> None:
         options = list(self.get(x, y))
@@ -25,29 +46,37 @@ class Grid:
         tile = choice(options)
         self.get(x, y).clear()
         self.get(x, y).add(tile)
-        self.collapse(x, y)
+        self.propagation_queue.append((x, y))
 
     def intersect(self, x: int, y: int, other_x: int, other_y: int, side) -> None:
         options = self.get(other_x, other_y)
         if len(options) == 0:
             return
 
-        connectors = [x.sides[side] for x in self.get(x, y)]
+        connectors = {x.sides[side] for x in self.get(x, y)}
 
         old_len = len(options)
 
         options.intersection_update(
-            [x for x in options if x.sides[side ^ 2] in connectors])
+            {x for x in options if x.sides[side ^ 2] in connectors})
 
         if old_len != len(options):
-            self.collapse(other_x, other_y)
+            self.propagation_queue.append((other_x, other_y))
 
-    def collapse(self, x: int, y: int) -> None:
-        if y > 0:
-            self.intersect(x, y, x, y - 1, 0)
-        if y < self.height - 1:
-            self.intersect(x, y, x, y + 1, 2)
-        if x > 0:
-            self.intersect(x, y, x - 1, y, 3)
-        if x < self.width - 1:
-            self.intersect(x, y, x + 1, y, 1)
+    def propagate(self):
+        while len(self.propagation_queue):
+            (x, y) = self.propagation_queue.pop()
+            if self.wrap_around:
+                self.intersect(x, y, x, (y - 1) % self.height, 0)
+                self.intersect(x, y, x, (y + 1) % self.height, 2)
+                self.intersect(x, y, (x - 1) % self.width, y, 3)
+                self.intersect(x, y, (x + 1) % self.width, y, 1)
+            else:
+                if y > 0:
+                    self.intersect(x, y, x, y - 1, 0)
+                if y < self.height - 1:
+                    self.intersect(x, y, x, y + 1, 2)
+                if x > 0:
+                    self.intersect(x, y, x - 1, y, 3)
+                if x < self.width - 1:
+                    self.intersect(x, y, x + 1, y, 1)
--- main.py
+++ main.py
@@ -17,34 +17,24 @@ tiles.add(Tile('├', [1, 1, 1, 0]))
 tiles.add(Tile('┤', [1, 0, 1, 1]))
 tiles.add(Tile('┬', [0, 1, 1, 1]))
 tiles.add(Tile('┴', [1, 1, 0, 1]))
-tiles.add(Tile('┴', [1, 1, 0, 1]))
-tiles.add(Tile('╶', [0, 1, 0, 0]))
-tiles.add(Tile('╷', [0, 0, 1, 0]))
-tiles.add(Tile('═', [0, 2, 0, 2]))
-tiles.add(Tile('║', [2, 0, 2, 0]))
-tiles.add(Tile('╬', [2, 2, 2, 2]))
-tiles.add(Tile('╜', [2, 0, 0, 1]))
-tiles.add(Tile('╓', [0, 1, 2, 0]))
-tiles.add(Tile('╫', [2, 1, 2, 1]))
-tiles.add(Tile('╪', [1, 2, 1, 2]))
+# tiles.add(Tile('╶', [0, 1, 0, 0]))
+# tiles.add(Tile('╷', [0, 0, 1, 0]))
+# tiles.add(Tile('═', [0, 2, 0, 2]))
+# tiles.add(Tile('║', [2, 0, 2, 0]))
+# tiles.add(Tile('╬', [2, 2, 2, 2]))
+# tiles.add(Tile('╜', [2, 0, 0, 1]))
+# tiles.add(Tile('╓', [0, 1, 2, 0]))
+# tiles.add(Tile('╫', [2, 1, 2, 1]))
+# tiles.add(Tile('╪', [1, 2, 1, 2]))
 
 
-grid = Grid(50, 20, tiles)
+grid = Grid(120, 30, tiles)
 
 # List contains entropy sorted references to grid tiles
 sorted_grid: list[set[Tile]] = sorted(grid.grid, key=len)
 
-while True:
-    sorted_grid.sort(key=len)
-    while len(sorted_grid) != 0 and len(sorted_grid[0]) <= 1:
-        sorted_grid.pop(0)
-
-    if len(sorted_grid) == 0:
-        break
-
-    option = sorted_grid.pop(0)
-    i = grid.grid.index(option)
-    grid.observe(i % grid.width, i // grid.width)
+while grid.observe_least_entropy():
+    grid.propagate()
 
 for y in range(grid.height):
     for x in range(grid.width):
